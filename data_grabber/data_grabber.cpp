710b9852acf12fd99abaa35e3bbf103787990008
Edward Zhang <edzhang@cs.washington.edu>
2014-01-30 04:43:51 -0500

    Converted sample to depth and rgb image viewing

--- data_grabber/data_grabber.cpp
+++ data_grabber/data_grabber.cpp
@@ -1,37 +1,87 @@
 #include <iostream>
 #include <string>
 #include <pcl/io/openni2_grabber.h>
-#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/visualization/image_viewer.h>
+
+#define BUFFER_SIZE 640*480
 
 using namespace std;
+using namespace pcl;
+using namespace openni_wrapper;
 
 class DataGrabber {
     public:
-        DataGrabber () :  path("data/"), viewer("Empty Room Data Grabber") {}
-        DataGrabber (string outputpath) : path(outputpath), viewer("Empty Room Data Grabber") {}
+        DataGrabber () :  path("data/"), rgbviewer("RGB Image") , depthviewer("Depth Image") {
+            rgbviewer.setSize(640, 480);
+            depthviewer.setSize(640, 480);
+            rgbviewer.setPosition(640,0);
+            depthbuffer = new unsigned short[BUFFER_SIZE];
+            rgbbuffer = new unsigned char[3*BUFFER_SIZE];
+        }
+        DataGrabber (string outputpath) : path(outputpath), rgbviewer("RGB Image") , depthviewer("Depth Image") {
+            rgbviewer.setSize(640, 480);
+            depthviewer.setSize(640, 480);
+            rgbviewer.setPosition(640,0);
+            depthbuffer = new unsigned short[BUFFER_SIZE];
+            rgbbuffer = new unsigned char[3*BUFFER_SIZE];
+        }
+        ~DataGrabber() {
+            delete [] depthbuffer;
+            delete [] rgbbuffer;
+        }
 
-        void cloud_cb_(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr &cloud) {
-            if (!viewer.wasStopped()) {
-                viewer.showCloud(cloud);
+        void data_cb_(const Image::Ptr& rgb, const DepthImage::Ptr& depth, float constant) {
+            boost::mutex::scoped_lock lock(image_mutex_);
+            image_ = rgb;
+            depthimage_ = depth;
+            if (rgb->getEncoding() != Image::RGB) {
+                image_->fillRGB(rgb->getWidth(), rgb->getHeight(), rgbbuffer);
             }
+            depthimage_->fillDepthImageRaw(depth->getWidth(), depth->getHeight(), depthbuffer);
         }
 
         void run() {
-            pcl::Grabber* grabber = new pcl::OpenNIGrabber();
+            OpenNIGrabber* grabber = new OpenNIGrabber();
 
-            boost::function<void(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr&)> f =
-                boost::bind(&DataGrabber::cloud_cb_, this, _1);
+            boost::function<void(const Image::Ptr&, const DepthImage::Ptr&, float)> f =
+                boost::bind(&DataGrabber::data_cb_, this, _1, _2, _3);
             grabber->registerCallback(f);
+            grabber->getDevice()->setExposure(900);
             grabber->start();
-            while(!viewer.wasStopped()) {
-                boost::this_thread::sleep(boost::posix_time::milliseconds(500));
+            while(!rgbviewer.wasStopped() && !depthviewer.wasStopped()) {
+                Image::Ptr image;
+                DepthImage::Ptr depthimage;
+                if (image_mutex_.try_lock()) {
+                    image_.swap(image);
+                    depthimage_.swap(depthimage);
+                    image_mutex_.unlock();
+                }
+                if (image && depthimage) {
+                    if (image->getEncoding() == Image::RGB) {
+                        rgbviewer.addRGBImage((const unsigned char*) image->getMetaData().getData(), image->getWidth(), image->getHeight());
+                    } else {
+                        rgbviewer.addRGBImage(rgbbuffer, image->getWidth(), image->getHeight());
+                    }
+                    depthviewer.addShortImage(depthbuffer, image->getWidth(), image->getHeight(),0,7000);
+                    depthviewer.spinOnce();
+                    rgbviewer.spinOnce();
+                }
             }
+            if (!rgbviewer.wasStopped()) rgbviewer.close();
+            if (!depthviewer.wasStopped()) depthviewer.close();
             grabber->stop();
         }
 
     private:
+        unsigned short* depthbuffer;
+        unsigned char* rgbbuffer;
         string path;
-        pcl::visualization::CloudViewer viewer;
+        visualization::ImageViewer depthviewer;
+        visualization::ImageViewer rgbviewer;
+
+        Image::Ptr image_;
+        DepthImage::Ptr depthimage_;
+        boost::mutex image_mutex_;
 };
 
 int main(int argc, char** argv) {
