b7941783c8b29a7c8a2fa1a2c28832ab6f7e9b96
Edward Zhang <edzhang@cs.washington.edu>
2014-06-24 17:58:17 -0700

    Filter RANSACed planes

--- globalreg/findplanes.cpp
+++ globalreg/findplanes.cpp
@@ -10,13 +10,36 @@
 #include <pcl/segmentation/sac_segmentation.h>
 
 const double ANGLETHRESHOLD = M_PI/9;
-const double NOISETHRESHOLD = 0.008;
-
+const double NOISETHRESHOLD = 0.01;
+const double MININLIERPROPORTION = 0.07;
 
 using namespace std;
 using namespace Eigen;
 using namespace pcl;
 
+double calculateEdgeProportion(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, vector<int> indices)
+{
+    vector<uint8_t> labels(cloud->size(), -1);
+    for (int i = 0; i < indices.size(); ++i) {
+        int x = indices[i]%cloud->width;
+        int y = indices[i]/cloud->width;
+
+        labels[indices[i]] = 0;
+        if (x != cloud->width-1 && labels[indices[i]+1])
+            labels[indices[i]+1] = 1;
+        if (x && labels[indices[i]-1])
+            labels[indices[i]-1] = 1;
+        if (y != cloud->height-1 && labels[indices[i]+cloud->width])
+            labels[indices[i]+cloud->width] = 1;
+        if (y && labels[indices[i]-cloud->width])
+            labels[indices[i]-cloud->width] = 1;
+    }
+    int count = 0;
+    for (int i = 0; i < labels.size(); ++i) {
+        if (labels[i] == 1) ++count;
+    }
+    return count/(double) indices.size();
+}
 void findPlanesRANSAC(
         pcl::PointCloud<pcl::PointXYZ>::Ptr cloud,
         std::vector<Eigen::Vector4d>& planes, std::vector<int>& ids)
@@ -38,28 +61,37 @@ void findPlanesRANSAC(
     }
 
     int n = 0;
-    while (filtered->size() > 0.2*cloud->size()) {
+    for (int z = 0; z < 10 && filtered->size() > MININLIERPROPORTION*cloud->size(); ++z) {
         // Find planes, if any
         seg.setInputCloud(filtered);
         seg.segment(*inliers, *coef);
         if (inliers->indices.size() == 0) {
             break;
         }
-
-        // Add plane to list
-        for (int i = 0; i < inliers->indices.size(); ++i) {
-            int idx = filtered->at(inliers->indices[i]).label;
-            ids[idx] = n;
+        // Filter planes
+        // A plane is valid if it takes up a large proportion of the points
+        // in the cloud, and if it has many large contiguous sections.
+        if (inliers->indices.size() > MININLIERPROPORTION*cloud->size()) {
+            vector<int> originalindices;
+            for (int i = 0; i < inliers->indices.size(); ++i) {
+                originalindices.push_back(filtered->at(inliers->indices[i]).label);
+            }
+            double edgeprop = calculateEdgeProportion(cloud, originalindices);
+            if (edgeprop < 0.04) {
+                // Add plane to list
+                for (int i = 0; i < originalindices.size(); ++i) {
+                    ids[originalindices[i]] = n;
+                }
+                Vector4d p(coef->values[0], coef->values[1], coef->values[2], coef->values[3]);
+                planes.push_back(p);
+                // Remove points on plane
+                extract.setInputCloud(filtered);
+                extract.setIndices(inliers);
+                extract.setNegative(true);
+                extract.filter(*filtered);
+                n++;
+            }
         }
-        Vector4d p(coef->values[0], coef->values[1], coef->values[2], coef->values[3]);
-        planes.push_back(p);
-
-        // Remove points on plane
-        extract.setInputCloud(filtered);
-        extract.setIndices(inliers);
-        extract.setNegative(true);
-        extract.filter(*filtered);
-        n++;
     }
 }
 
@@ -132,6 +164,5 @@ void findPlanes(
 {
     ids.resize(cloud->size());
     for (int i = 0; i < ids.size(); ++i) ids[i] = -1;
-    return findPlanesWithNormals(cloud, planes, ids);
-    //return findPlanesRANSAC(cloud, planes, ids);
+    return findPlanesRANSAC(cloud, planes, ids);
 }
