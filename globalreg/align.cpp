af53809b2db118f36f166e6858d2a7a0da8f7a49
Edward Zhang <edzhang@cs.washington.edu>
2014-06-26 01:38:33 -0700

    Added plane overlap to interaction

--- globalreg/align.cpp
+++ globalreg/align.cpp
@@ -1,9 +1,7 @@
 #include "pairwise.h"
 #include "findplanes.h"
+#include <pcl/common/transforms.h>
 #include <pcl/io/io.h>
-#include <pcl/io/grabber.h>
-#include <pcl/io/openni2_grabber.h>
-#include <pcl/io/openni2/openni.h>
 #include <pcl/io/vtk_lib_io.h>
 #include <pcl/point_types.h>
 #include <pcl/visualization/pcl_visualizer.h>
@@ -12,30 +10,23 @@ using namespace pcl;
 using namespace std;
 using namespace Eigen;
 
-int main(int argc, char** argv) {
-    if (argc < 3) {
-        cout << "Usage: align cloud1.pcd cloud2.pcd" << endl;
-        return 0;
-    }
-    visualization::PCLVisualizer viewer("Cloud viewer");
-    PointCloud<PointXYZ>::Ptr cloud1(new PointCloud<PointXYZ>);
-    PointCloud<PointXYZ>::Ptr cloud2(new PointCloud<PointXYZ>);
-    io::loadPCDFile<PointXYZ>(argv[1], *cloud1);
-    io::loadPCDFile<PointXYZ>(argv[2], *cloud2);
-    vector<Vector4d> srcplanes, tgtplanes;
-    vector<int> srcids, tgtids;
-    cout << "Finding planes..." << endl;
-    findPlanes(cloud1, srcplanes, srcids);
-    cout << "Done finding frame 1 planes" << endl;
-    findPlanes(cloud2, tgtplanes, tgtids);
-    cout << "Done finding planes" << endl;
+int colormode = 0;
+visualization::PCLVisualizer* viewer;
+vector<Vector4d> srcplanes, tgtplanes;
+vector<int> srcids, tgtids;
+
+PointCloud<PointXYZ>::Ptr cloud1(new PointCloud<PointXYZ>);
+PointCloud<PointXYZ>::Ptr cloud2(new PointCloud<PointXYZ>);
+PointCloud<PointXYZRGB>::Ptr colored1(new PointCloud<PointXYZRGB>);
+PointCloud<PointXYZRGB>::Ptr colored2(new PointCloud<PointXYZRGB>);
+visualization::PointCloudColorHandlerRGBField<PointXYZRGB> rgb1(colored1);
+visualization::PointCloudColorHandlerRGBField<PointXYZRGB> rgb2(colored2);
+
+void colorPlaneCorrespondences() {
     vector<int> planecorrespondences;
-
     int numcorrespondences = findPlaneCorrespondences(cloud1, cloud2, srcplanes, srcids, tgtplanes, tgtids, planecorrespondences);
-    cout << numcorrespondences << endl;
+    cout << numcorrespondences << " plane correspondences found" << endl;
 
-    PointCloud<PointXYZRGB>::Ptr colored1(new PointCloud<PointXYZRGB>);
-    copyPointCloud(*cloud1, *colored1);
     for (int i = 0; i < srcids.size(); ++i) {
         if (srcids[i] == -1) {
             colored1->at(i).r = 255;
@@ -56,8 +47,6 @@ int main(int argc, char** argv) {
             }
         }
     }
-    PointCloud<PointXYZRGB>::Ptr colored2(new PointCloud<PointXYZRGB>);
-    copyPointCloud(*cloud2, *colored2);
     for (int i = 0; i < tgtids.size(); ++i) {
         if (tgtids[i] == -1) {
             colored2->at(i).r = 127;
@@ -69,13 +58,145 @@ int main(int argc, char** argv) {
             colored2->at(i).b = 0;
         }
     }
-    visualization::PointCloudColorHandlerRGBField<PointXYZRGB> rgb1(colored1);
-    visualization::PointCloudColorHandlerRGBField<PointXYZRGB> rgb2(colored2);
-    viewer.addPointCloud<PointXYZRGB>(colored1, rgb1, "Mesh1");
-    viewer.addPointCloud<PointXYZRGB>(colored2, rgb2, "Mesh2");
+}
 
-    while (!viewer.wasStopped()) {
-        viewer.spinOnce(100);
+void colorPlanes() {
+    for (int i = 0; i < srcids.size(); ++i) {
+        if (srcids[i] == -1) {
+            colored1->at(i).r = 255;
+            colored1->at(i).g = 255;
+            colored1->at(i).b = 255;
+        } else if (srcids[i] == 0) {
+            colored1->at(i).r = 255;
+            colored1->at(i).g = 0;
+            colored1->at(i).b = 0;
+        } else if (srcids[i] == 1) {
+            colored1->at(i).r = 0;
+            colored1->at(i).g = 255;
+            colored1->at(i).b = 0;
+        } else if (srcids[i] == 2) {
+            colored1->at(i).r = 0;
+            colored1->at(i).g = 0;
+            colored1->at(i).b = 255;
+        } else if (srcids[i] == 3) {
+            colored1->at(i).r = 255;
+            colored1->at(i).g = 0;
+            colored1->at(i).b = 255;
+        } else if (srcids[i] == 4) {
+            colored1->at(i).r = 0;
+            colored1->at(i).g = 255;
+            colored1->at(i).b = 255;
+        } else if (srcids[i] == 5) {
+            colored1->at(i).r = 255;
+            colored1->at(i).g = 255;
+            colored1->at(i).b = 0;
+        } else {
+            colored1->at(i).r = 127;
+            colored1->at(i).g = 127;
+            colored1->at(i).b = 127;
+        }
+    }
+    for (int i = 0; i < tgtids.size(); ++i) {
+        if (tgtids[i] == -1) {
+            colored2->at(i).r = 127;
+            colored2->at(i).g = 127;
+            colored2->at(i).b = 127;
+        } else if (tgtids[i] == 0) {
+            colored2->at(i).r = 127;
+            colored2->at(i).g = 0;
+            colored2->at(i).b = 0;
+        } else if (tgtids[i] == 1) {
+            colored2->at(i).r = 0;
+            colored2->at(i).g = 127;
+            colored2->at(i).b = 0;
+        } else if (tgtids[i] == 2) {
+            colored2->at(i).r = 0;
+            colored2->at(i).g = 0;
+            colored2->at(i).b = 127;
+        } else if (tgtids[i] == 3) {
+            colored2->at(i).r = 127;
+            colored2->at(i).g = 0;
+            colored2->at(i).b = 127;
+        } else if (tgtids[i] == 4) {
+            colored2->at(i).r = 0;
+            colored2->at(i).g = 127;
+            colored2->at(i).b = 127;
+        } else if (tgtids[i] == 5) {
+            colored2->at(i).r = 127;
+            colored2->at(i).g = 127;
+            colored2->at(i).b = 0;
+        } else {
+            colored2->at(i).r = 63;
+            colored2->at(i).g = 63;
+            colored2->at(i).b = 63;
+        }
+    }
+}
+
+void kbd_cb(const visualization::KeyboardEvent& event, void*) {
+    if (event.keyDown()) {
+        if (event.getKeyCode() == ' ') {
+            colormode = (colormode+1)%4;
+            if (colormode == 0) {           // Planes only
+                colorPlanes();
+            } else if (colormode == 1) {    // Plane Correspondences
+                colorPlaneCorrespondences();
+            } else if (colormode == 2) {    // Hide plane, draw other points
+
+            } else if (colormode == 3) {
+            }
+            viewer->updatePointCloud<PointXYZRGB>(colored1, rgb1, "Mesh1");
+            viewer->updatePointCloud<PointXYZRGB>(colored2, rgb2, "Mesh2");
+        } else if (event.getKeyCode() == ',') {
+            static bool view1 = true;
+            view1 = !view1;
+            viewer->setPointCloudRenderingProperties(visualization::PCL_VISUALIZER_OPACITY, view1?1:0, "Mesh1");
+        } else if (event.getKeyCode() == '.') {
+            static bool view2 = true;
+            view2 = !view2;
+            viewer->setPointCloudRenderingProperties(visualization::PCL_VISUALIZER_OPACITY, view2?1:0, "Mesh2");
+        } else if (event.getKeyCode() == 'z') {
+            static bool donealign = false;
+            if (!donealign) {
+                vector<int> planecorrespondences;
+                findPlaneCorrespondences(cloud1, cloud2, srcplanes, srcids, tgtplanes, tgtids, planecorrespondences);
+                for (int i = 0; i < planecorrespondences.size(); ++i) {
+                    if (planecorrespondences[i] > -1) {
+                        Matrix4d t = overlapPlanes(srcplanes[i], tgtplanes[planecorrespondences[i]]);
+                        transformPointCloud(*colored1, *colored1, t);
+                        viewer->updatePointCloud<PointXYZRGB>(colored1, rgb1, "Mesh1");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+
+int main(int argc, char** argv) {
+    if (argc < 3) {
+        cout << "Usage: align cloud1.pcd cloud2.pcd" << endl;
+        return 0;
+    }
+    io::loadPCDFile<PointXYZ>(argv[1], *cloud1);
+    io::loadPCDFile<PointXYZ>(argv[2], *cloud2);
+    cout << "Finding planes..." << endl;
+    findPlanes(cloud1, srcplanes, srcids);
+    cout << "Done finding frame 1 planes" << endl;
+    findPlanes(cloud2, tgtplanes, tgtids);
+    cout << "Done finding planes" << endl;
+    vector<int> planecorrespondences;
+    viewer = new visualization::PCLVisualizer("Cloud viewer");
+
+    copyPointCloud(*cloud1, *colored1);
+    copyPointCloud(*cloud2, *colored2);
+    colorPlanes();
+    viewer->addPointCloud<PointXYZRGB>(colored1, rgb1, "Mesh1");
+    viewer->addPointCloud<PointXYZRGB>(colored2, rgb2, "Mesh2");
+    viewer->registerKeyboardCallback(kbd_cb, NULL);
+
+    while (!viewer->wasStopped()) {
+        viewer->spinOnce(100);
     }
     return 0;
 }
