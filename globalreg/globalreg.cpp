4c4981e43b05964ad25e1f12e4cf15c5a54c3aa3
Edward Zhang <edzhang@cs.washington.edu>
2014-07-13 03:29:17 -0700

    Improved globalreg app
    Writes transforms
    Fixed bug in complete point cloud
    Doesn't have to perform full alignment (can read in xforms)

--- globalreg/globalreg.cpp
+++ globalreg/globalreg.cpp
@@ -20,7 +20,7 @@ using namespace Eigen;
 
 typedef PointXYZ Pt;
 vector<PointCloud<Pt>::Ptr > clouds;
-vector<Matrix4d> xforms;
+vector<Matrix4d> xforms, cumxforms;
 bool displaypairs = false;
 int displayindex = 0;
 bool ready = false;
@@ -30,12 +30,12 @@ PointCloud<Pt>::Ptr lastcopy(new PointCloud<Pt>);
 PointCloud<Pt>::Ptr complete(new PointCloud<Pt>);
 
 void generateComplete() {
-    complete->clear();
+    PointCloud<Pt>::Ptr tmp(new PointCloud<Pt>);
     for (int i = 0; i < clouds.size(); ++i) {
-        *complete += *clouds[i];
+        *tmp += *clouds[i];
     }
     VoxelGrid<Pt> vgf;
-    vgf.setInputCloud(complete);
+    vgf.setInputCloud(tmp);
     vgf.setLeafSize(0.01f,0.01f,0.01f);
     vgf.filter(*complete);
 }
@@ -88,13 +88,15 @@ void keyboard_callback(const visualization::KeyboardEvent& event, void* viewer) 
     }
 }
 
-void loadFile(string fname, PointCloud<PointXYZ>::Ptr cloud) {
+void loadFile(string fname, PointCloud<PointXYZ>::Ptr cloud, bool smoothing) {
     io::loadPCDFile<PointXYZ>(fname, *cloud);
-    FastBilateralFilter<PointXYZ> fbf;
-    fbf.setSigmaS(9);
-    fbf.setSigmaR(0.05);
-    fbf.setInputCloud(cloud);
-    fbf.applyFilter(*cloud);
+    if (smoothing) {
+        FastBilateralFilter<PointXYZ> fbf;
+        fbf.setSigmaS(9);
+        fbf.setSigmaR(0.05);
+        fbf.setInputCloud(cloud);
+        fbf.applyFilter(*cloud);
+    }
     for (int i = 0; i < cloud->size(); ++i) {
         if (isnan(cloud->at(i).x)) cloud->at(i).z = cloud->at(i).x;
     }
@@ -102,14 +104,24 @@ void loadFile(string fname, PointCloud<PointXYZ>::Ptr cloud) {
 
 int main(int argc, char** argv) {
     if (argc < 3) {
-        cout << "Usage: globalreg MANIFEST prefix" << endl;
+        cout << "Usage: globalreg MANIFEST -outputprefix PREFIX" << endl;
+        cout << "       globalreg MANIFEST -xforms XFORMPATTERN" << endl;
         return 0;
     }
-    string prefix = argv[2];
+    string outputprefix = "";
+    string xformspattern = "";
     bool subsample = false;
     bool display = true;
+    bool smoothing = true;
     if (console::find_switch(argc, argv, "-subsample")) subsample = true;
     if (console::find_switch(argc, argv, "-nodisplay")) display = false;
+    if (console::find_switch(argc, argv, "-nosmoothing")) smoothing = false;
+    if (console::find_argument(argc, argv, "-outputprefix") > -1) {
+        console::parse_argument(argc, argv, "-outputprefix", outputprefix);
+    }
+    if (console::find_argument(argc, argv, "-xforms") > -1) {
+        console::parse_argument(argc, argv, "-xforms", xformspattern);
+    }
     try {
         ifstream in(argv[1]);
         string s;
@@ -118,7 +130,7 @@ int main(int argc, char** argv) {
         while (!in.eof()) {
             PointCloud<Pt>::Ptr cloud(new PointCloud<Pt>);
             //io::loadPCDFile<Pt>(s, *cloud);
-            loadFile(s, cloud);
+            loadFile(s, cloud, smoothing);
             cout << "Read point cloud " << n << endl;
             clouds.push_back(cloud);
             getline(in,s);
@@ -130,14 +142,28 @@ int main(int argc, char** argv) {
     }
     cout << "Done reading point clouds" << endl;
     xforms.push_back(Matrix4d::Identity());
+    cumxforms.push_back(Matrix4d::Identity());
     vector<Vector4d> srcplanes, tgtplanes;
     vector<int> srcids, tgtids;
     for (int i = 1; i < clouds.size(); ++i) {
         srcplanes.clear(); tgtplanes.clear();
         srcids.clear(); tgtids.clear();
-        Matrix4d t = align(clouds[i], clouds[i-1], srcplanes, srcids, tgtplanes, tgtids).transform;
-        xforms.push_back(xforms.back()*t);
-        cout << "Done alignment " << i << endl;
+        Matrix4d t;
+        if (xformspattern.length() > 0) {
+            char fname[20];
+            sprintf(fname, xformspattern.c_str(), i);
+            ifstream in(fname);
+            for (int i = 0; i < 4; ++i) {
+                for (int j = 0; j < 4; ++j) {
+                    in >> t(i,j);
+                }
+            }
+        } else {
+            t = align(clouds[i], clouds[i-1], srcplanes, srcids, tgtplanes, tgtids).transform;
+            cout << "Done alignment " << i << endl;
+        }
+        cumxforms.push_back(cumxforms.back()*t);
+        xforms.push_back(t);
     }
 
     if (display) {
@@ -145,7 +171,7 @@ int main(int argc, char** argv) {
         char cloudname[10];
         for (int i = 0; i < clouds.size(); ++i) {
             sprintf(cloudname, "cloud%03d", i);
-            transformPointCloud(*clouds[i], *clouds[i], xforms[i]);
+            transformPointCloud(*clouds[i], *clouds[i], cumxforms[i]);
             viewer.addPointCloud<Pt>(clouds[i], cloudname);
             viewer.setPointCloudRenderingProperties(visualization::PCL_VISUALIZER_COLOR, i%3==0?1:0, i%3==1?1:0, i%3==2?1:0, cloudname);
             viewer.setPointCloudRenderingProperties(visualization::PCL_VISUALIZER_OPACITY, 0.0, cloudname);
@@ -160,13 +186,19 @@ int main(int argc, char** argv) {
             viewer.spinOnce(100);
         }
     }
-    PLYWriter w;
-    char fname[20];
-    for (int i = 0; i < clouds.size(); ++i) {
-        sprintf(fname, (prefix + "%04d.ply").c_str(), i);
-        w.write<Pt>(fname, *clouds[i], true, false);
-        //*complete += *clouds[i];
-        //clouds[i].reset(); // Free up memory(?)
+    if (outputprefix.length() > 0) {
+        PLYWriter w;
+        char fname[20];
+        for (int i = 0; i < clouds.size(); ++i) {
+            sprintf(fname, (outputprefix + "%04d.ply").c_str(), i);
+            w.write<Pt>(fname, *clouds[i], true, false);
+            sprintf(fname, (outputprefix + "%04d.xform").c_str(), i);
+            ofstream out(fname);
+            out << xforms[i] << endl;
+            out.close();
+            //*complete += *clouds[i];
+            //clouds[i].reset(); // Free up memory(?)
+        }
     }
     //PLYWriter w;
     //w.write<Pt>(argv[2], *complete, true, false);
