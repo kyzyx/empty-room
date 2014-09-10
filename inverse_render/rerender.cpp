b704074bd3750fe50c606c796d1c1f283e733615
Edward Zhang <edzhang@cs.washington.edu>
2014-09-10 15:08:51 -0700

    Improved radiance file output

--- inverse_render/rerender.cpp
+++ inverse_render/rerender.cpp
@@ -11,25 +11,46 @@ void outputRadianceFile(string filename, WallFinder& wf, Mesh& m, InverseRender&
     out << " 0 0" << endl << endl;
 
     // Output light materials
+    // RADIANCE light parameters are in radiance; for diffuse surfaces the radiance L
+    // is related to the radiosity J by
+    // J = pi*L
     for (int i = 0; i < ir.lights.size(); ++i) {
         out << "void light l" << i << endl << 0 << endl << 0 << endl << 3 << " ";
-        out << ir.lights[i](0) << " " << ir.lights[i](1) << " " << ir.lights[i](2) << endl << endl;
+        out << ir.lights[i](0)/M_PI << " " << ir.lights[i](1)/M_PI << " " << ir.lights[i](2)/M_PI << endl << endl;
     }
 
     // Output light triangles;
     for (int i = 0; i < m.getMesh()->NFaces(); ++i) {
+        R3MeshFace* f = m.getMesh()->Face(i);
+        int lightid = -1;
+        // Check if face is a light
+        for (int j = 0; j < 3; ++j) {
+            int n = m.getMesh()->VertexID(m.getMesh()->VertexOnFace(f, j));
+            if (m.labels[n] < 0) {
+                lightid = -1;
+                break;
+            } else {
+                if (lightid < 0) lightid = m.labels[n];
+                else if (lightid != m.labels[n]) {
+                    lightid = -1;
+                    break;
+                }
+            }
+        }
+        if (lightid == -1) continue;
+        out << "l" << lightid << " polygon light" << i << endl << "0 0 9" << endl;
+        for (int j = 0; j < 3; ++j) {
+            double b[3];
+            b[0] = 0; b[1] = 0; b[2] = 0;
+            b[j] = 1;
+            R3Point v = m.getMesh()->FaceBarycentric(f, b);
+            out << v[0] << " " << v[1] << " " << v[2] << " ";
+        }
+        out << endl << endl;
     }
 
     double hi = wf.ceilplane;
     double lo = wf.floorplane;
-    double eps = 0.00001;
-
-    out << "l0 polygon light0\n0 0 12\n" \
-        "1.1 1.6 6  1.7 1.6 6 1.7 1.0 6 1.1 1.0 6" << endl << endl;
-
-    out << "l1 polygon light1\n0 0 12\n" \
-        "1.85 " << hi-eps << " 1 2.15 " << hi-eps << " 1 2.15 " << hi-eps << " 3 1.85 " << hi-eps << " 3" << endl << endl;
-
 
     // output walls
     for (int i = 0; i < wf.wallsegments.size(); ++i) {
@@ -48,11 +69,10 @@ void outputRadianceFile(string filename, WallFinder& wf, Mesh& m, InverseRender&
     double zmin = m.getMesh()->BBox().ZMin();
     double xmax = m.getMesh()->BBox().XMax();
     double zmax = m.getMesh()->BBox().ZMax();
-    // output floor
+    // Output floor and ceiling
     out << "wallmat polygon ceil" << endl << 0 << endl << 0 << endl << 12 << " ";
     out << xmin << " " << hi << " " << zmin << " " << xmin << " " << hi << " " << zmax << " ";
     out << xmax << " " << hi << " " << zmax << " " << xmax << " " << hi << " " << zmin << endl << endl;
-    // output ceiling
     out << "wallmat polygon floor" << endl << 0 << endl << 0 << endl << 12 << " ";
     out << xmin << " " << lo << " " << zmin << " " << xmax << " " << lo << " " << zmin << " ";
     out << xmax << " " << lo << " " << zmax << " " << xmin << " " << lo << " " << zmax << endl << endl;
