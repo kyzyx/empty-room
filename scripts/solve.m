5c81e5b2ccd3f18a3b7c6129ffc0dc44f596581e
Edward Zhang <edzhang@cs.washington.edu>
2014-10-13 23:59:55 -0700

    Fix bugs in matlab optimization

--- scripts/solve.m
+++ scripts/solve.m
@@ -11,19 +11,19 @@ function y = optimization(p, A, B, C, weights)
 end
 
 function inliers = ransacInlierFunction(params, data, maxpercenterr)
-    err = abs(params(1)*(data(:,4:end)*params(2:end) + data(:,3)) - data(:,2));
+    err = abs(params(1)*(data(:,4:end)*params(2:end)' + data(:,3)) - data(:,2));
     percenterr = err./data(:,2);
     inliers = find(percenterr < maxpercenterr);
 end
 function params = ransacComputeParams(data)
     [p,l,r] = solvechannel(data(:,4:end), data(:,2), data(:,3), data(:,1));
-    params = [p, l];
+    params = [p, l'];
 end
 function [p,l,r] = solvechannelransac(A,B,C,weights,iters,mpe)
     nlightparams = 1;
     nlights = size(A,2);
     data = [weights B C A];
-    [params,r] = ransac(@ransacComputeParams, data, iters, (2+nlightparams*nlights), @(p,d) ransacInlierFunction(p,d,mpe));
+    [params,r] = ransac(@ransacComputeParams, data, iters, (4+nlightparams*nlights), @(p,d) ransacInlierFunction(p,d,mpe));
     p = params(1);
     l = params(2:end);
     r = 1 - (r/size(data,1));
@@ -31,14 +31,9 @@ end
 
 filename = 'eq.m';
 run(filename);
-[p0,l0,r0] = solvechannelransac(A,B0,C0,weights,800,0.05);
-[p1,l1,r1] = solvechannelransac(A,B1,C1,weights,800,0.05);
-[p2,l2,r2] = solvechannelransac(A,B2,C2,weights,800,0.05);
-%[p0,l0,r0] = solvechannel(A,B0,C0,weights);
-%[p1,l1,r1] = solvechannel(A,B1,C1,weights);
-%[p2,l2,r2] = solvechannel(A,B2,C2,weights);
-
-%fprintf('MRSE: %.3f %.3f %.3f\n', r0,r1,r2);
+[p0,l0,r0] = solvechannelransac(A,B0,C0,weights,900,0.05);
+[p1,l1,r1] = solvechannelransac(A,B1,C1,weights,900,0.05);
+[p2,l2,r2] = solvechannelransac(A,B2,C2,weights,900,0.05);
 fprintf('Outlier proportion: %.3f %.3f %.3f\n', r0,r1,r2);
 fprintf('Reflectance: %.3f %.3f %.3f\n', p0, p1, p2);
 for i=1:length(l0),
