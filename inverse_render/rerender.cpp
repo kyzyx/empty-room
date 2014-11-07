50b21a39dbf5b587766fdb1f2b619f400d10551a
Edward Zhang <edzhang@cs.washington.edu>
2014-11-06 17:28:31 -0800

    Basic pbrt file output
    No lights or floor textures yet

--- inverse_render/rerender.cpp
+++ inverse_render/rerender.cpp
@@ -222,4 +222,86 @@ void outputPlyFile(std::string filename, WallFinder& wf, Mesh& m, InverseRender&
 
 }
 void outputPbrtFile(std::string filename, WallFinder& wf, Mesh& m, InverseRender& ir) {
+    ofstream out(filename);
+    ifstream tpl("template.pbrt");
+    // Basic rendering info
+    string line;
+    do {
+        getline(tpl, line);
+        out << line << endl;
+    } while(!tpl.eof());
+    out << endl;
+    // Output wall material
+    out << "WorldBegin" << endl;
+    out << "MakeNamedMaterial \"Wall\"" << endl;
+    out << "\t\"color Kd\" [";
+    for (int i = 0; i < 3; ++i) out << ir.wallMaterial(i) << " ";
+    out << "]" << endl;
+    out << "\t\"float sigma\" [0.0]" << endl;
+    out << "\t\"string type\" [\"matte\"]" << endl << endl;
+    // Output room geometry
+    out << "AttributeBegin" << endl;
+    out << "Transform [1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 1]" << endl;
+    out << "NamedMaterial \"Wall\"" << endl;
+    out << "Shape \"trianglemesh\"" << endl;
+    out << "\"point P\" [" << endl;
+    R3Box b;
+    for (int i = 0; i < wf.wallsegments.size(); ++i) {
+        Eigen::Vector3f p[] = {
+            wf.getWallEndpoint(i,0,1),
+            wf.getWallEndpoint(i,0,0)
+        };
+        Eigen::Vector3f c = wf.getNormalizedWallEndpoint(i,0,0);
+        R3Point pp(c(0),c(1),c(2));
+        b.Union(pp);
+        for (int j = 0; j < 2; ++j) {
+            out << '\t' << p[j](0) << " " << p[j](1) << " " << p[j](2) << endl;
+        }
+    }
+    for (int i = 0; i < 8; ++i) {
+        Vector4f p(b.Coord(i&1,0),((i>>2)&1)?wf.floorplane:wf.ceilplane,b.Coord((i>>1)&1,2),1);
+        p = wf.getNormalizationTransform().inverse()*p;
+        out <<  '\t' << p(0)/p(3) << " " << p(1)/p(3) << " " << p(2)/p(3) << endl;
+    }
+    out << "]" << endl;
+    out << "\"integer indices\" [" << endl;
+    for (int i = 0; i < wf.wallsegments.size(); ++i) {
+        if (i == wf.wallsegments.size() - 1) {
+            out << '\t' << 2*i << " " << 0 << " " << 2*i+1 << endl;
+            out << '\t' << 0 << " " << 1 << " " << 2*i+1 << endl;
+        } else {
+            out << '\t' << 2*i << " " << 2*i+2 << " " << 2*i+1 << endl;
+            out << '\t' << 2*i+2 << " " << 2*i+3 << " " << 2*i+1 << endl;
+        }
+    }
+    int n = wf.wallsegments.size()*2;
+    // Floor triangles
+    out << '\t' << n << " " << n+2 << " " << n+1 << endl;
+    out << '\t' << n+1 << " " << n+2 << " " << n+3 << endl;
+    // Ceiling triangles
+    out << '\t' << n+4 << " " << n+5 << " " << n+6 << endl;
+    out << '\t' << n+5 << " " << n+7 << " " << n+6 << endl;
+    out << "]" << endl;
+    out << "\"bool generatetangents\" [\"false\"]" << endl;
+    out << "\"string name\" [\"Room\"]" << endl;
+    out << "AttributeEnd" << endl;
+    // FIXME: Separate floor material
+
+    // Output light sources
+    for (int i = 0; i < ir.lights.size(); ++i) {
+        out << "AttributeBegin" << endl;
+        out << "Transform [1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 1]" << endl;
+        out << "AreaLightSource \"diffuse\" \"rgb L\" [";
+        for (int j = 0; j < 3; ++j) out << ir.lights[i](j) << " ";
+        out << "]" << endl;
+        out << "Shape \"trianglemesh\"" << endl;
+        out << "\"point P\" [" << endl;
+        // FIXME
+        out << "]" << endl;
+        out << "\"integer indices\" [" << endl;
+        // FIXME
+        out << "]" << endl;
+        out << "AttributeEnd" << endl;
+    }
+    out << "WorldEnd" << endl;
 }
