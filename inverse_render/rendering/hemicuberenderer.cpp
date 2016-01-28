468815fb4f454f93b20882a4309177dab78f307b
Edward Zhang <edzhang@cs.washington.edu>
2016-01-28 04:25:36 -0800

    Revert most of a4258cf

--- inverse_render/rendering/hemicuberenderer.cpp
+++ inverse_render/rendering/hemicuberenderer.cpp
@@ -94,7 +94,6 @@ float HemicubeRenderer::renderHemicube(
             for (int j = 0; j < res; ++j) {
                 int visibility = ftoi(light[3*(i*res+j)]);
                 int lightid = ftoi(light[3*(i*res+j)+1]);
-                lightid = 0;
                 if (lightid > 0) {
                     lightid--;
                     if (lightid >= lightareas.size()) lightareas.resize(lightid+1);
@@ -115,7 +114,6 @@ float HemicubeRenderer::renderHemicube(
         for (int j = 0; j < res; ++j) {
             int visibility = ftoi(light[3*(i*res+j)]);
             int lightid = ftoi(light[3*(i*res+j)+1]);
-                lightid = 0;
             if (lightid > 0) {
                 lightid--;
                 if (lightid >= lightareas.size()) lightareas.resize(lightid+1);
