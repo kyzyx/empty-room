22035a5212611ac8b0a0bf3c3ad201d0f6b7c71b
Edward Zhang <edzhang@cs.washington.edu>
2016-04-26 23:30:22 -0700

    Add 'OR' to convert for window compositing

--- inverse_render/utils/convert.cpp
+++ inverse_render/utils/convert.cpp
@@ -9,6 +9,7 @@
 using namespace std;
 
 float image[3*2560*1440];
+float im2[3*2560*1440];
 inline unsigned char fclamp(float f) {
     if (f > 255) return 255;
     if (f < 0) return 0;
@@ -25,13 +26,15 @@ std::vector<std::string> &split(const std::string &s, char delim, std::vector<st
 
 int main(int argc, char** argv) {
     if (argc < 3) {
-        cout << "Usage: convertimage inputimage outputformat [-intensityscale outputscale] [-colorscale scaler,scaleg,scaleb] [-scale scale] [-flip_y] [-flip_x]" << endl;
+        cout << "Usage: convertimage inputimage outputimage [-intensityscale outputscale] [-colorscale scaler,scaleg,scaleb] [-scale scale] [-flip_y] [-flip_x] [-or inputimage2]" << endl;
         return 0;
     }
     double intensityscale[3];
     float scale = 1;
     float gamma = 0;
     bool linear = false;
+    string orfile = "";
+    bool do_or = false;
     for (int i = 0; i < 3; i++) intensityscale[i] = 1;
     if (pcl::console::find_argument(argc, argv, "-intensityscale") >= 0) {
         double d;
@@ -54,6 +57,10 @@ int main(int argc, char** argv) {
     if (pcl::console::find_switch(argc, argv, "-linear")) {
         linear = true;
     }
+    if (pcl::console::find_argument(argc, argv, "-or") >= 0) {
+        pcl::console::parse_argument(argc, argv, "-or", orfile);
+        do_or = true;
+    }
     bool flip_y = false;
     bool flip_x = false;
     if (pcl::console::find_switch(argc, argv, "-flip_y")) {
@@ -67,16 +74,27 @@ int main(int argc, char** argv) {
         cout << "Error reading file" << endl;
         return 0;
     }
+    if (do_or && !ImageIO::readFloatImage(orfile.c_str(), im2, w, h)) {
+        cout << "Error reading file" << endl;
+        return 0;
+    }
     if (flip_x || flip_y) ImageIO::flip(image, w, h, sizeof(float)*3, flip_x, flip_y);
     //string fmt = argv[2];
     //string outfile = ImageIO::replaceExtension(argv[1], fmt);
     string outfile = argv[2];
     string fmt = outfile.substr(outfile.find_last_of(".")+1);
     for (int i = 0; i < w*h; i++) {
+        bool iszero = true;
         for (int j = 0; j < 3; j++) {
             if (gamma > 0) image[3*i+j] = pow(image[3*i+j], gamma);
             image[3*i+j] *= intensityscale[j];
             if (!linear && gamma > 0) image[3*i+j] = pow(image[3*i+j], 1./gamma);
+            if (image[3*i+j]) iszero = false;
+        }
+        if (do_or && iszero) {
+            for (int j = 0; j < 3; j++) {
+                image[3*i+j] = im2[3*i+j];
+            }
         }
     }
     cv::Mat im(h, w, CV_32FC3, image);
